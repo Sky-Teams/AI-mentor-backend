a5ebca2ad81838554ae689a2c9adfaef78115c3f
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-04-27 05:23:20 -0700

    Implement schema validation

---
+++ backend/src/modules/citations/interface/citation.schema.ts
@@ -0,0 +1,52 @@
+import { string, z } from "zod";
+
+const AuthorSchema = z.object({
+  firstName: z.string().min(1),
+  lastName: z.string().min(1),
+});
+
+const BookSchema = z.object({
+  type: z.literal("BOOK"),
+  publisher: z.string().min(1),
+  placePublished: z.string().optional(),
+  isbn: z.string().optional(),
+  page: z.string(),
+  volumeNumber: z.number().optional(),
+  edition: z.string().optional(),
+});
+
+const WebsiteSchema = z.object({
+  type: z.literal("WEBSITE"),
+  url: z.string().url(),
+  websiteName: z.string().min(1),
+  dateAccess: z.string().optional(),
+});
+
+const JournalSchema = z.object({
+  type: z.literal("JOURNAL"),
+  journalName: z.string().min(1),
+  volumeNumber: z.number().optional(),
+  issueNumber: z.number().optional(),
+  page: z.string().min(1),
+  doi: z.string().optional(),
+});
+const ReportSchema = z.object({
+  type: z.literal("REPORT"),
+  publisher: z.string().optional(),
+  url: z.string().optional(),
+});
+const BaseCitation = z.object({
+  authors: z.array(AuthorSchema).min(1),
+  title: z.string().min(1),
+  datePublished: z.string().min(1),
+});
+
+export const CreateCitationSchema = z.object({
+  citation: z.discriminatedUnion("type", [
+    BaseCitation.merge(BookSchema),
+    BaseCitation.merge(WebsiteSchema),
+    BaseCitation.merge(JournalSchema),
+    BaseCitation.merge(ReportSchema),
+  ]),
+  style: z.enum(["APA", "MLA"]),
+});
