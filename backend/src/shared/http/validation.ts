0f29f589fdf5cd17936177004c79ea865bcc26cf
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-05-03 05:48:40 -0700

    fix validation change flatten to issues

--- backend/src/shared/http/validation.ts
+++ backend/src/shared/http/validation.ts
@@ -19,7 +19,10 @@ export const validate =
             "Request validation failed.",
             StatusCodes.UNPROCESSABLE_ENTITY,
             "VALIDATION_ERROR",
-            error.errors.map((err) => err.path.join(".")),
+            error.issues.map((err) => ({
+              field: err.path.join("."),
+              message: err.message,
+            })),
           ),
         );
         return;
