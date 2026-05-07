e68d1f9af446cba233584221dc45641adf77d988
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-05-07 09:53:08 +0430

    Create PrismaJournalRepository to find a journal by id #26

--- backend/src/modules/journal/infrastructure/prisma-journal.repository.ts
+++ backend/src/modules/journal/infrastructure/prisma-journal.repository.ts
@@ -5,8 +5,14 @@ export class PrismaJournalRepository implements JournalRepository {
   public constructor(private readonly prisma: PrismaClient) {}
 
   public async findAll(): Promise<Array<{ id: string; name: string }>> {
-    return this.prisma.journal.findMany({
-      select: { id: true, name: true },
+    return this.prisma.journal.findMany();
+  }
+
+  public async findById(
+    id: string,
+  ): Promise<{ id: string; name: string } | null> {
+    return this.prisma.journal.findFirst({
+      where: { id },
     });
   }
 }
