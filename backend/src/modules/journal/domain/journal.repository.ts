085b40fffd7c1acf598a9a0405ec661d70879084
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-05-07 09:42:30 +0430

    Create JournalRepository  findById method #26

--- backend/src/modules/journal/domain/journal.repository.ts
+++ backend/src/modules/journal/domain/journal.repository.ts
@@ -1,6 +1,4 @@
-import { Journal } from "@prisma/client";
-
 export interface JournalRepository {
   findAll(): Promise<Array<{ id: string; name: string }>>;
-  // findById(id: string): Promise<{ id: string; name: string }>;
+  findById(id: string): Promise<{ id: string; name: string } | null>;
 }
