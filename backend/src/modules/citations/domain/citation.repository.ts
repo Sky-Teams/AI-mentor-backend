d0547a6a3e9aed607ed9df849cb6af0448d976d1
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-05-11 00:21:49 -0700

    Implement get and delete method in repository

--- backend/src/modules/citations/domain/citation.repository.ts
+++ backend/src/modules/citations/domain/citation.repository.ts
@@ -1,9 +1,22 @@
-import { Citation } from "./citation";
+import { CitationFormatType } from "@prisma/client";
+import { Citation, CitationOutput, CitationType } from "./citation";
 
 export interface CitationRepository {
   createCitaiotn(input: {
     citation: Citation;
     ownerId: string;
     projectId: string;
+    style: CitationFormatType;
   }): Promise<void>;
+  GetCitation(projectId: string, ownerId: string): Promise<CitationOutput[]>;
+  GetCitationType(input: CitationOutput): Promise<{
+    citation: Citation;
+    type: CitationType;
+    style: CitationFormatType;
+  }>;
+  DeleteCitation(citationId: string, ownerId: string): Promise<void>;
+  GetCitationById(
+    citationId: string,
+    ownerId: string,
+  ): Promise<CitationOutput | null>;
 }
