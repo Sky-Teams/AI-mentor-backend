9503629b870d0c11d1498919d18a451a6dfc37d2
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-06-03 21:03:55 +0430

    Create createJournal repository #51

--- backend/src/modules/journal/domain/journal.repository.ts
+++ backend/src/modules/journal/domain/journal.repository.ts
@@ -1,4 +1,7 @@
+import { JournalDefinition } from "src/shared/seed-data/journals.js";
+
 export interface JournalRepository {
   findAll(): Promise<Array<{ id: string; name: string }>>;
   findById(id: string): Promise<{ id: string; name: string } | null>;
+  createJournal(journal: JournalDefinition): Promise<JournalDefinition>;
 }
