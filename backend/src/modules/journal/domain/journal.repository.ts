f11284bba5b57fd39cf6b65796c4e1317faa5824
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-06-04 08:45:28 +0430

    Add CreatedJournal type #51

--- backend/src/modules/journal/domain/journal.repository.ts
+++ backend/src/modules/journal/domain/journal.repository.ts
@@ -1,7 +1,33 @@
-import { JournalDefinition } from "src/shared/seed-data/journals.js";
+import { Journal } from "@prisma/client";
+import {
+  CreateJournalInput,
+  JournalDefinition,
+} from "src/shared/seed-data/journals.js";
+
+export type CreatedJournal = JournalDefinition & {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  sections: Array<
+    JournalDefinition["sections"][number] & {
+      id: string;
+      journalId: string;
+      createdAt: Date;
+      updatedAt: Date;
+      checklist: Array<
+        JournalDefinition["sections"][number]["checklist"][number] & {
+          id: string;
+          journalSectionTemplateId: string;
+          createdAt: Date;
+          updatedAt: Date;
+        }
+      >;
+    }
+  >;
+};
 
 export interface JournalRepository {
   findAll(): Promise<Array<{ id: string; name: string }>>;
   findById(id: string): Promise<{ id: string; name: string } | null>;
-  createJournal(journal: JournalDefinition): Promise<JournalDefinition>;
+  createJournal(input: CreateJournalInput): Promise<Journal>;
 }
