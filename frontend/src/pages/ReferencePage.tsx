944308bf834ffcc64f5564b2c4f9609d0d68865d
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-06-12 12:26:11 -0700

    Fix some issue and use formatter Api

--- frontend/src/pages/ReferencePage.tsx
+++ frontend/src/pages/ReferencePage.tsx
@@ -1,88 +1,105 @@
 import { useState } from "react";
 import { ReferenceSearchPanel } from "../components/referenceSearchPanel";
 import {
+  CreateReferenceInput,
   Reference,
+  referenceApi,
   ReferenceStyle,
+  ReferenceStyles,
   ReferenceTypes,
 } from "../services/api/reference";
-import { referenceFormatterService } from "../services/formatters/referenceFormatterService";
 
 export const ReferencePage = () => {
-  const [isOpenSeacrhBox, setIsOpenSearchBox] = useState(false);
-  const [savedReference, setSavedReference] = useState<
-    { id: string; text: string; raw: any; type: string }[]
+  const [isSearchBoxOpen, setIsSearchBoxOpen] = useState(false);
+  const [saveReferences, setSaveReferences] = useState<
+    { id: string; text: string; raw: Reference; type: ReferenceTypes }[]
   >([]);
   const [currentStyle, setCurrentStyle] = useState<ReferenceStyle>("APA");
   const [errorMessage, setErrorMessage] = useState("");
-  const formatStyle = ["APA", "MLA", "VANCOUVER"];
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleSaveReference = async (
-    selectedReference: Reference,
-    searchType: ReferenceTypes,
-  ) => {
+  const handleAddReference = async (references: CreateReferenceInput) => {
     try {
-      const formattedText = await referenceFormatterService.format(
-        selectedReference,
-        searchType,
-        currentStyle,
-      );
+      setIsLoading(true);
+      const [formattedText] = await referenceApi.formatReference({
+        references: [
+          {
+            reference: references.reference,
+            type: references.type,
+          },
+        ],
+        style: currentStyle,
+      });
+
       const newReference = {
-        id: selectedReference.id || Date.now().toString(),
+        id: references.reference.id,
         text: formattedText,
-        raw: selectedReference,
-        type: searchType,
+        raw: references.reference,
+        type: references.type,
       };
 
-      setSavedReference((prevReference) => [...prevReference, newReference]);
-      setIsOpenSearchBox(false);
+      setSaveReferences((prev) => [...prev, newReference]);
+      setIsSearchBoxOpen(false);
     } catch (error: any) {
-      setErrorMessage(error?.response?.data?.error?.message);
-      console.log(error);
+      setErrorMessage(
+        error?.response?.data?.error?.message || "Failed to add reference",
+      );
+    } finally {
+      setIsLoading(false);
     }
   };
 
   const handleStyleChange = async (newStyle: ReferenceStyle) => {
     try {
       setCurrentStyle(newStyle);
+      setIsLoading(true);
 
-      const updatedReferenceStyle = await Promise.all(
-        savedReference.map(async (item) => {
-          const newFormattedText = await referenceFormatterService.format(
-            item.raw,
-            item.type as ReferenceTypes,
-            newStyle,
-          );
-          return { ...item, text: newFormattedText };
-        }),
-      );
-      setSavedReference(updatedReferenceStyle);
+      const formattedTexts = await referenceApi.formatReference({
+        references: saveReferences.map((item) => ({
+          reference: item.raw,
+          type: item.type,
+        })),
+        style: newStyle,
+      });
+
+      const updateFormatStyles = saveReferences.map((item, index) => {
+        return {
+          ...item,
+          text: formattedTexts[index],
+        };
+      });
+      setSaveReferences(updateFormatStyles);
     } catch (error: any) {
-      setErrorMessage(error?.response?.data?.error?.message);
-      console.log(error);
+      setErrorMessage(
+        error?.response?.data?.error?.message || "Failed to format reference",
+      );
+    } finally {
+      setIsLoading(false);
     }
   };
+
   return (
     <div>
+      {errorMessage && <p className="error-text">{errorMessage}</p>}
+
       <div className="page-header">
         <div className="button-row">
           <button
             className="primary-button"
-            onClick={() => setIsOpenSearchBox(true)}
+            onClick={() => setIsSearchBoxOpen(true)}
           >
             New Citation
           </button>
           <select
             className="modern-select"
-            name=""
-            id=""
             value={currentStyle}
             onChange={(e) =>
               handleStyleChange(e.target.value as ReferenceStyle)
             }
           >
-            {formatStyle.map((item, index) => (
-              <option value={item} key={index}>
-                {item}
+            {ReferenceStyles.map((item, index) => (
+              <option value={item} key={index} disabled={isLoading}>
+                {isLoading ? "Loading..." : item}
               </option>
             ))}
           </select>
@@ -90,16 +107,16 @@ export const ReferencePage = () => {
       </div>
 
       <ReferenceSearchPanel
-        isOpen={isOpenSeacrhBox}
-        onClose={() => setIsOpenSearchBox(false)}
-        onSaveReference={handleSaveReference}
+        isOpen={isSearchBoxOpen}
+        onClose={() => setIsSearchBoxOpen(false)}
+        onSaveReference={(reference) => handleAddReference(reference)}
       />
 
       <div className="card">
         <h3>References</h3>
-        {savedReference && savedReference.length > 0 ? (
+        {saveReferences.length > 0 ? (
           <ul className="saved-references-list">
-            {savedReference.map((item, index) => {
+            {saveReferences.map((item, index) => {
               return <li key={item.id || index}>{item.text}</li>;
             })}
           </ul>
