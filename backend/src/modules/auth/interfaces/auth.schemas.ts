930e5214d7f92dfd673c51217732228ac0c29647
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-06-15 09:10:46 -0700

    add token schema

--- backend/src/modules/auth/interfaces/auth.schemas.ts
+++ backend/src/modules/auth/interfaces/auth.schemas.ts
@@ -14,3 +14,7 @@ export const loginSchema = z.object({
 export const refreshSchema = z.object({
   refreshToken: z.string().min(1),
 });
+
+export const verifiedTokenSchema = z.object({
+  token: z.string().min(1),
+});
