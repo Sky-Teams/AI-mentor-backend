b230d778c05214355552f0160da00a092b3e14d0
hamidullah-hamidi <hamidullahh754@gmail.com>
2026-06-22 17:07:30 +0430

    Create updateJournalSchema #74

--- backend/src/modules/journal/interface/journal.schema.ts
+++ backend/src/modules/journal/interface/journal.schema.ts
@@ -52,3 +52,54 @@ export const createJournalSchema = z.object({
 export const specialtyIdQuerySchema = z.object({
   specialtyId: z.string().cuid().optional(),
 });
+
+export const updateJournalSchema = z.object({
+  name: z.string().min(1).max(180).optional(),
+  publisher: z.string().min(1).max(180).optional(),
+  description: z.string().min(1).max(1000).optional(),
+  guidelinePack: z.string().min(1).optional(),
+  specialtyId: z.string().min(1).optional(),
+  sections: z
+    .array(
+      z.object({
+        title: z.string().min(1).max(180).optional(),
+        sectionOrder: z.number().int().min(1).optional(),
+        isOptional: z.boolean().optional().optional(),
+        maxChars: z.number().min(1).optional(),
+        description: z.string().min(1).max(1000).optional(),
+        checklists: z
+          .array(
+            z.object({
+              title: z.string().min(1).max(180).nullable().optional(),
+              items: z.array(z.string().min(1).max(500)).min(1).optional(),
+            }),
+          )
+          .optional(),
+        subsections: z
+          .array(
+            z.object({
+              title: z.string().min(1).max(180).optional(),
+              sectionOrder: z.number().int().min(1).optional(),
+              isOptional: z.boolean().optional(),
+              maxChars: z.number().min(1).optional(),
+              description: z.string().min(1).max(1000).optional(),
+              checklists: z
+                .array(
+                  z.object({
+                    title: z.string().min(1).max(180).nullable().optional(),
+                    items: z
+                      .array(z.string().min(1).max(500))
+                      .min(1)
+                      .optional(),
+                  }),
+                )
+                .optional()
+                .default([]),
+            }),
+          )
+          .optional(),
+      }),
+    )
+    .min(1)
+    .optional(),
+});
