8d0b7585a7653551dced9ed1f25f02deba7ac9e2
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-06-27 08:59:06 +0430

    Implement controller for user

---
+++ backend/src/modules/users/interfaces/user.controller.ts
@@ -0,0 +1,26 @@
+import { StatusCodes } from "http-status-codes";
+import { UserService } from "../application/user.service";
+import type { Request, Response } from "express";
+import { successResponse } from "src/shared/http/api-response";
+
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  public async changePassword(
+    request: Request,
+    response: Response,
+  ): Promise<void> {
+    const { currentPassword, newPassword } = request.body as {
+      currentPassword: string;
+      newPassword: string;
+    };
+
+    const result = await this.userService.changePassword(
+      request.auth!.userId,
+      currentPassword,
+      newPassword,
+    );
+
+    response.status(StatusCodes.OK).json(successResponse(result));
+  }
+}
