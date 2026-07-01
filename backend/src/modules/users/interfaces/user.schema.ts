5b6ad72bac8d42d1132cb874050f8ce2adcf726a
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-01 10:43:31 +0430

    add validation for update profile

--- backend/src/modules/users/interfaces/user.schema.ts
+++ backend/src/modules/users/interfaces/user.schema.ts
@@ -4,3 +4,7 @@ export const changePasswordSchema = z.object({
   currentPassword: z.string().min(8),
   newPassword: z.string().min(8),
 });
+
+export const updateProfileSchema = z.object({
+  fullName: z.string().min(1),
+});
