b3b699dc54563f878edfd5ae0b0ea99670f44921
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-06 18:15:34 +0430

    Add formatPage and some minor changes in MLA format

--- backend/src/modules/references/infrastructure/formatters/mla.formatter.ts
+++ backend/src/modules/references/infrastructure/formatters/mla.formatter.ts
@@ -27,22 +27,18 @@ export class MLAFormatter {
   private async formatJournal(c: JournalSearchResponse) {
     let authors = await this.formatAuthors(c?.authors);
 
-    if (authors.endsWith("et al.")) {
-      authors = authors.substring(0, authors.length - 1);
-    }
+    const page = c?.page ? ` ${await this.formatMLAPages(c.page)},` : "";
+    const volume = c?.volume ? ` vol. ${c.volume},` : "";
+    const issue = c?.issue ? ` no. ${c.issue},` : "";
+    const year = c.datePublished ? ` ${await getYear(c?.datePublished)},` : "";
+    const doiPart = c?.doi ? `  ${c.doi}.` : "";
+    const title = c?.title ? ` "${c.title}." ` : "";
+    const journalName = c?.journalName ? ` <i>${c.journalName},</i>` : "";
 
-    const page = c?.page ? `pp. ${c.page}, ` : "";
-    const volume = c?.volume ? `vol. ${c.volume}, ` : "";
-    const issue = c?.issue ? `no. ${c.issue}, ` : "";
-    const year = await getYear(c?.datePublished);
-    const doiPart = c?.doi ? `  ${c.doi}` : "";
-    const title = c?.title ? `"${c.title}." ` : "";
-    const journalName = c?.journalName ? `${c.journalName}, ` : "";
-
-    return `${authors}${title}${journalName}${volume}${issue}${year ? year + ", " : ""}${page}${doiPart}.`;
+    return `${authors}${title}${journalName}${volume}${issue}${year}${page}${doiPart}`;
   }
 
-  public async formatAuthors(authors: any): Promise<string> {
+  private async formatAuthors(authors: any): Promise<string> {
     if (!authors) return "";
 
     const authorsArray = Array.isArray(authors) ? authors : [authors];
@@ -53,6 +49,7 @@ export class MLAFormatter {
     const initials = authorsArray.slice(0, 3).map((author) => {
       const firstName =
         author.firstName
+          .trim()
           ?.split(" ")
           ?.map((n: string) => n.charAt(0).toUpperCase() + n.slice(1))
           ?.join(" ") || "";
@@ -60,14 +57,43 @@ export class MLAFormatter {
       return firstName;
     });
     if (count === 1) {
-      return `${authorsArray[0].lastName}, ${initials[0]}. `;
+      return `${authorsArray[0].lastName}, ${initials[0]}.`;
     }
 
     if (count === 2) {
-      return `${authorsArray[0].lastName}, ${initials[0]}, and ${initials[1]} ${authorsArray[1].lastName}. `;
+      return `${authorsArray[0].lastName}, ${initials[0]}, and ${initials[1]} ${authorsArray[1].lastName}.`;
     }
 
-    return `${authorsArray[0].lastName}, ${initials[0]}, et al. `;
+    return `${authorsArray[0].lastName}, ${initials[0]}, et al.`;
   }
 
+  private async formatMLAPages(pages: any) {
+    if (!pages) return "";
+    if (!pages.includes("-")) return `p. ${pages}`;
+
+    const formatPages = pages.trim().split("-");
+
+    if (
+      formatPages.length === 2 &&
+      formatPages[0].length !== formatPages[1].length
+    )
+      return `pp. ${pages}`;
+
+    if (formatPages.length === 2 && formatPages[0] === formatPages[1]) {
+      console.log("length 2");
+      return `p. ${formatPages[0]}`;
+    }
+
+    if (formatPages.length > 2) return `p. ${formatPages[0]}+`;
+
+    let sharedIndex = 0;
+    while (
+      sharedIndex < formatPages[0].length &&
+      formatPages[0][sharedIndex] === formatPages[1][sharedIndex]
+    ) {
+      sharedIndex++;
+    }
+
+    return `pp. ${formatPages[0]}-${formatPages[1].slice(sharedIndex)}`;
+  }
 }
