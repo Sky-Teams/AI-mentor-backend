e3ccfdf3289b8ccf2f900b89100bd29c818ce262
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-19 16:51:14 +0430

    Add harvard format style to formatter service

--- backend/src/modules/references/application/reference.formatter.service.ts
+++ backend/src/modules/references/application/reference.formatter.service.ts
@@ -4,12 +4,14 @@ import { APAFormatter } from "../infrastructure/formatters/APAFormatter";
 import { MLAFormatter } from "../infrastructure/formatters/MLAFormatter";
 import { VancouverFormatter } from "../infrastructure/formatters/VancouverFormatter";
 import { StatusCodes } from "http-status-codes";
+import { HarvardFormatter } from "../infrastructure/formatters/harvard.formatter";
 
 export class ReferenceFormatterService {
   constructor(
     private readonly apa: APAFormatter,
     private mla: MLAFormatter,
     private vancouver: VancouverFormatter,
+    private harvard: HarvardFormatter,
   ) {}
 
   format(reference: Reference, type: ReferenceTypes, style: ReferenceStyle) {
@@ -20,6 +22,8 @@ export class ReferenceFormatterService {
         return this.mla.format(reference, type);
       case "VANCOUVER":
         return this.vancouver.format(reference, type);
+      case "HARVARD":
+        return this.harvard.format(reference, type);
       default:
         throw new AppError(
           "Unsupported format style",
