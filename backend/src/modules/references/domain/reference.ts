548e829308be4bba5c852a3fb587a399bfad9eb8
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-07-21 11:19:14 +0430

    Add new style to ReferenceStyles interface

--- backend/src/modules/references/domain/reference.ts
+++ backend/src/modules/references/domain/reference.ts
@@ -2,7 +2,16 @@ export const ReferenceValue = ["JOURNAL"] as const;
 
 export type ReferenceTypes = (typeof ReferenceValue)[number];
 
-export const ReferenceStyles = ["APA", "MLA", "VANCOUVER"] as const;
+export const ReferenceStyles = [
+  "APA",
+  "MLA",
+  "VANCOUVER",
+  "HARVARD",
+  "IEEE",
+  "CHICAGO_AUTHOR_DATE",
+  "CHICAGO_FULL_NOTE",
+  "OSCOLA",
+] as const;
 export type ReferenceStyle = (typeof ReferenceStyles)[number];
 
 export interface Authors {
@@ -12,6 +21,7 @@ export interface Authors {
 
 export interface JournalSearchResponse {
   id: string;
+  journalNameAbbrev?: string | null;
   publisher?: string | null;
   doi?: string | null;
   issue?: string | null;
