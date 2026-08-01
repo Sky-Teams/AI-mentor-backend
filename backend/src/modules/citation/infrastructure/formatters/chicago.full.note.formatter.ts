f14e4355b476d2af93578a81d754c800f6f33b36
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-08-01 19:45:10 +0430

    Implement chicago full note

---
+++ backend/src/modules/citation/infrastructure/formatters/chicago.full.note.formatter.ts
@@ -0,0 +1,12 @@
+import { Reference } from "src/modules/references/domain/reference";
+
+export class ChicagoFullNoteCitationFormatter {
+    constructor(
+        private readonly 
+    ){}
+
+  public formatCitation(reference: Reference, sectionId: string) {
+
+    return "";
+  }
+}
