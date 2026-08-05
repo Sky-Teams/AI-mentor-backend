5fe45c3bbfec956b8151878705b726edc2bc9bfa
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-08-05 13:34:00 +0430

    Add Chicago full note style

--- backend/src/modules/citation/infrastructure/formatters/chicago.full.note.formatter.ts
+++ backend/src/modules/citation/infrastructure/formatters/chicago.full.note.formatter.ts
@@ -1,12 +1,43 @@
 import { Reference } from "src/modules/references/domain/reference";
+import { formatPage, getYear } from "src/shared/utils/format-citation-helper";
 
 export class ChicagoFullNoteCitationFormatter {
-    constructor(
-        private readonly 
-    ){}
+  public formatCitation(reference: Reference): { footnote: string } {
+    let footNotes = "";
+    if (reference.authors && reference.authors.length !== 0)
+      footNotes += `${this.formatAuthors(reference.authors)},`;
+    if (reference.title) footNotes += ` "${reference.title},"`;
+    if (reference.journalName) footNotes += ` ${reference.journalName}`;
 
-  public formatCitation(reference: Reference, sectionId: string) {
+    if (reference.volume) footNotes += ` ${reference.volume}`;
+    if (reference.issue) footNotes += `, no. ${reference.issue}`;
 
-    return "";
+    if (reference.datePublished)
+      footNotes += ` (${getYear(reference.datePublished)})`;
+
+    if (reference.page) footNotes += `: ${formatPage(reference.page)}`;
+
+    if (footNotes) footNotes += ".";
+
+    return {
+      footnote: footNotes,
+    };
+  }
+
+  private formatAuthors(authors: any) {
+    if (!authors) return "";
+
+    const authorsArray = authors ? Object.values(authors) : [];
+    if (authorsArray.length === 0) return "";
+
+    const formatted = authorsArray.map((author: any) => {
+      return `${author.firstName} ${author.lastName}`;
+    });
+
+    if (formatted.length === 1) return formatted[0];
+
+    const lastAuthor = formatted.pop();
+
+    return `${formatted.join(", ")}, and ${lastAuthor}`;
   }
 }
