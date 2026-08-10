f939f2a5c27b5d0aca98ef9542c4e11617e9a750
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-08-10 19:45:16 +0430

    Add cron job for subscription expire

---
+++ backend/src/jobs/subscription-job.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import cron from "node-cron";
+import { SubscriptionService } from "src/modules/subscription/application/subscription.service";
+import { PrismaSubscriptionRepository } from "src/modules/subscription/infrastructure/prisma-subscription.repository";
+import { PrismaUserRepository } from "src/modules/users/infrastructure/prisma-user.repository";
+const prisma = new PrismaClient();
+
+const userRepository = new PrismaUserRepository(prisma);
+const subscriptionRepository = new PrismaSubscriptionRepository(prisma);
+
+const subscriptionService = new SubscriptionService(
+  subscriptionRepository,
+  userRepository,
+);
+
+export function startExpirePlansJob() {
+  cron.schedule("*/60 * * * *", async () => {
+    try {
+      console.log("Subscription job is running...");
+      const result = await subscriptionService.expirePlans();
+      console.log(result.message);
+    } catch (error) {
+      console.error("Expire plans job failed:", error);
+    }
+  });
+}
