c981022698f55d8aed697f6a9e142b06a7fa5105
SakinaEbrahimi <sakina.ibrahiey986@gmail.com>
2026-08-15 09:53:27 +0430

    Remove setInterval function and back to cron

--- backend/src/jobs/subscription-job.ts
+++ backend/src/jobs/subscription-job.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import cron from "node-cron";
 import { SubscriptionService } from "src/modules/subscription/application/subscription.service";
 import { PrismaSubscriptionRepository } from "src/modules/subscription/infrastructure/prisma-subscription.repository";
 import { PrismaUserRepository } from "src/modules/users/infrastructure/prisma-user.repository";
@@ -13,17 +14,16 @@ const subscriptionService = new SubscriptionService(
 );
 
 export function startExpirePlansJob() {
-  const intervalHours = Number(
-    process.env.SUBSCRIPTION_EXPIRE_SCHEDULE_TIME ?? 1,
-  );
-  const intervalMs = intervalHours * 60 * 60 * 1000;
-  setInterval(async () => {
+  const scheduleTime =
+    process.env.SUBSCRIPTION_EXPIRE_SCHEDULE_TIME ?? "0 0 * * *";
+
+  cron.schedule(scheduleTime, async () => {
     try {
       console.log("Subscription expiration job is running...");
       const result = await subscriptionService.expirePlans();
       console.log(result.message);
     } catch (error) {
-      console.log("Subscription expiration job failed:", error);
+      console.error("Expire plans job failed:", error);
     }
-  }, intervalMs);
+  });
 }
